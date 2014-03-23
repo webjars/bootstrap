5479fa5a1bb6bb2b427b17f2df4e9ea34200b9c2
Karim BENHDECH <karim.benhdech@spartacode.fr>
2014-03-23 17:46:49 +0100

    Repercussion following the official support for RequireJS in WebJars since version 2.2.1-2

--- src/main/resources/webjars-requirejs.js
+++ src/main/resources/webjars-requirejs.js
@@ -2,6 +2,8 @@
 
 // Ensure any request for this webjar brings in jQuery.
 requirejs.config({
-    paths: { "bootstrap": webjars.path("bootstrap", "js/bootstrap") },
-    shim: { "bootstrap": [ "jquery", "webjars!jquery.js" ] }
+  paths: { "bootstrap": webjars.path("bootstrap", "js/bootstrap") },
+  shim: { "bootstrap": [ "jquery" ] }
 });
+
+
