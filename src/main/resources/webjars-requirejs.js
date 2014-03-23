64ead691cc0858f5e47f1a7379721f3a97d78305
Karim BENHDECH <karim.benhdech@spartacode.fr>
2014-03-23 17:47:38 +0100

    Better compatibility with require-css

--- src/main/resources/webjars-requirejs.js
+++ src/main/resources/webjars-requirejs.js
@@ -2,8 +2,10 @@
 
 // Ensure any request for this webjar brings in jQuery.
 requirejs.config({
-  paths: { "bootstrap": webjars.path("bootstrap", "js/bootstrap") },
+  paths: { 
+    "bootstrap": webjars.path("bootstrap", "js/bootstrap"),
+    "bootstrap-css": webjars.path("bootstrap", "css/bootstrap")  
+  },
   shim: { "bootstrap": [ "jquery" ] }
 });
-
 
